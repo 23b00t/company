702379812a73dcdb6ab60c1ce3de29702ecbf04f
Daniel Kipp <daniel.kipp@gmail.com>
2024-10-15 12:31:27 +0200

    fixed warning

--- views/navigation.php
+++ views/navigation.php
@@ -1,9 +1,17 @@
-<div style="margin: 8px; text-align: center;" >
-  <a href="index.php?area=employee&action=showTabelle" ><button class="btn btn-outline-success">Mitarbeiter</button></a>
-  <a href="index.php?area=employee&action=showForm" ><button class="btn btn-outline-success">Mitarbeiter hinzufügen</button></a>
+<?php
 
+/** @navbar */
+
+?>
+
+<div style="margin: 8px; text-align: center;" >
+  <!-- Emplyee links -->
+  <a href="index.php?area=employee&action=showTabelle" ><button class="btn btn-outline-success">Mitarbeiter</button></a>
+  <a href="index.php?area=employee&action=showForm" >
+    <button class="btn btn-outline-success">Mitarbeiter hinzufügen</button>
+  </a>
+
+  <!-- Car links -->
   <a href="index.php?area=car&action=showTabelle" ><button class="btn btn-outline-success">Autos</button></a>
   <a href="index.php?area=car&action=showForm" ><button class="btn btn-outline-success">Auto hinzufügen</button></a>
 </div>
-
-
