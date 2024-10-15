7df97bd6e4df898f3913d18f1d89fb1808c6b372
Daniel Kipp <daniel.kipp@gmail.com>
2024-10-15 11:00:57 +0200

    setting title

--- views/navigation.php
+++ views/navigation.php
@@ -1,9 +1,3 @@
-<?php
-
-namespace firma\views;
-
-?>
-
 <div style="margin: 8px; text-align: center;" >
   <a href="index.php?action=showTabelle" ><button class="btn btn-outline-success">Zur Tabelle</button></a>
   <!-- <a href="index.php?action=showEingabe" ><button>Zur Eingabe</button></a> -->
