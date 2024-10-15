f95da6f3e1fda5b27b3fa2c5463635886b92492d
Daniel Kipp <daniel.kipp@gmail.com>
2024-10-15 10:31:27 +0200

    style fix

--- views/navigation.php
+++ views/navigation.php
@@ -1,5 +1,13 @@
+<?php
+
+namespace firma\views;
+
+?>
+
 <div style="margin: 8px; text-align: center;" >
-	<a href="index.php?action=showTabelle" ><button class="btn btn-outline-success">Zur Tabelle</button></a>
-	<!-- <a href="index.php?action=showEingabe" ><button>Zur Eingabe</button></a> -->
-	<a href="index.php?action=showForm" ><button class="btn btn-outline-success">Mitarbeiter hinzufügen</button></a>
+  <a href="index.php?action=showTabelle" ><button class="btn btn-outline-success">Zur Tabelle</button></a>
+  <!-- <a href="index.php?action=showEingabe" ><button>Zur Eingabe</button></a> -->
+  <a href="index.php?action=showForm" ><button class="btn btn-outline-success">Mitarbeiter hinzufügen</button></a>
 </div>
+
+
