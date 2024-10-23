40fd2408cbd2d701fed656ec3b0a405d7ee3b48f
Daniel Kipp <daniel.kipp@gmail.com>
2024-10-23 15:12:05 +0200

    fixed base layout

--- views/application.html.php
+++ views/application.html.php
@@ -10,15 +10,15 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Rental Liste</title>
+    <title>Mitarbeiter Portal</title>
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css' rel='stylesheet'
       integrity='sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH' crossorigin='anonymous'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   </head>
   <body>
-  <div class="container">
-    <?php include __DIR__ . '/navigation.php'; ?>
-    <?php include __DIR__ . '/' . $area . '/' . $controller->getView() . '.php'; ?>
-  </div>
+    <div class="container">
+      <?php include __DIR__ . '/navigation.php'; ?>
+      <?php include __DIR__ . '/' . $area . '/' . $controller->getView() . '.php'; ?>
+    </div>
   </body>
 </html>
